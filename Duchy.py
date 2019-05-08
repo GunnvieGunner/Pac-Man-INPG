ef5360df6a76fd2d0377db1f4336d2a2e9caab28
Robert <robertsynoczek@gmail.com>
2019-05-08 23:16:29 +0200

    Basic Ghost sceleton

--- Duchy.py
+++ Duchy.py
@@ -4,4 +4,15 @@ import pygame
 
 class Ghost:
 
-    def __init__(self, name, sprite, ):
+    def __init__(self, name, sprite, velocity):
+
+        self.name = name
+        self.sprite = pygame.image.load(sprite)
+        self.velocity = velocity
+
+    def move(self):
+        pass
+
+    def draw(self):
+        pass
+
